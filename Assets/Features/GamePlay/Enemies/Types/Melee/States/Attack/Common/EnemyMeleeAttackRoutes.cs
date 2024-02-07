07286e6501d333c70c54f3bbeb86142c02bb3df7
Ivan Fedorchenko <noncasted@gmail.com>
2024-02-07 06:44:28 +0300

    wip

--- Assets/Features/GamePlay/Enemies/Types/Melee/States/Attack/Common/EnemyMeleeAttackRoutes.cs
+++ Assets/Features/GamePlay/Enemies/Types/Melee/States/Attack/Common/EnemyMeleeAttackRoutes.cs
@@ -1,28 +1,26 @@
-﻿using Features.GamePlay.Enemies.Entity.States.Common;
+﻿using Features.GamePlay.Enemies.Types.Melee.Common;
 using GamePlay.Enemies.Entity.Setup.Paths;
 
 namespace GamePlay.Enemies.Types.Melee.States.Attack.Common
 {
     public class EnemyMeleeAttackRoutes
     {
-        private const string Root = EnemyStatesRoutes.Root + "Types/Melee/";
-        
         public const string LocalName = EnemyAssetsPrefixes.State + "Attack_Local";
-        public const string LocalPath = Root + "Attack/Local";
+        public const string LocalPath = EnemyMeleeRoutes.Root + "Attack/Local";
 
         public const string RemoteName = EnemyAssetsPrefixes.State + "Attack_Remote";
-        public const string RemotePath = Root + "Attack/Remote";
+        public const string RemotePath = EnemyMeleeRoutes.Root + "Attack/Remote";
 
         public const string DefinitionName = EnemyAssetsPrefixes.Definition + "Attack";
-        public const string DefinitionPath = Root + "Attack/Definition";
+        public const string DefinitionPath = EnemyMeleeRoutes.Root + "Attack/Definition";
 
         public const string AnimationName = EnemyAssetsPrefixes.Animation + "Attack";
-        public const string AnimationPath = Root + "Attack/Animation";
+        public const string AnimationPath = EnemyMeleeRoutes.Root + "Attack/Animation";
         
         public const string ConfigName = EnemyAssetsPrefixes.Config + "Attack";
-        public const string ConfigPath = Root + "Attack/Config";
+        public const string ConfigPath = EnemyMeleeRoutes.Root + "Attack/Config";
 
         public const string GizmosConfigName = EnemyAssetsPrefixes.Config + "Attack_Gizmos";
-        public const string GizmosConfigPath = Root + "Attack/GizmosConfig";
+        public const string GizmosConfigPath = EnemyMeleeRoutes.Root + "Attack/GizmosConfig";
     }
 }
